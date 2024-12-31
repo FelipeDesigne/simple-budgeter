c845bb97f35b36b408e8687c48f9b42d117ccafa
FelipeDesigne <felipebdias98@gmail.com>
2024-12-31 17:58:45 -0300

    Alterando domínio do Supabase para .app

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const supabaseUrl = 'https://qqcmxpnmxpxmcxlvvxlk.supabase.co';
+const supabaseUrl = 'https://qqcmxpnmxpxmcxlvvxlk.supabase.app';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxY214cG5teHB4bWN4bHZ2eGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQwNTY3NzAsImV4cCI6MjAxOTYzMjc3MH0.Wd_bEzGxlNVXtZQz5JqFfqBWLQEcXHNXWBEwYxpgHBk';
 
 export const supabase = createClient<Database>(
